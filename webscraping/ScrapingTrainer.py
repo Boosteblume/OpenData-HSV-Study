521ddf19ea691caa31698749cdd9889bc3899937
Ghost911-s <maxblum94@yahoo.de>
2022-04-23 11:13:40 +0200

    Trainer are now in the db

--- webscraping/ScrapingTrainer.py
+++ webscraping/ScrapingTrainer.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
+import psycopg2
 
-#request try
 
 url = "https://www.weltfussball.de/teams/hamburger-sv/9/"
 
@@ -29,4 +29,47 @@ res = res[4:]
 
 chunks = [res[i:i+2] for i in range(0, len(res), 3)]
 
-print(chunks)
+chunks = chunks[:21]
+
+
+#connection settings
+host = "studyserverhh.postgres.database.azure.com"
+dbname = "postgres"
+user = "Footballstudy"
+password = "Abstieg2022"
+sslmode = "require"
+# Construct connection string
+conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(host, user, dbname, password, sslmode)
+conn = psycopg2.connect(conn_string) 
+print("Connection established")
+cursor = conn.cursor()
+
+
+#this function is to upload the football-data
+def trainer():
+    # Drop previous table of same name if one exists
+    cursor.execute("DROP TABLE IF EXISTS trainer;")
+    print("Finished dropping table (if existed)")
+
+
+     # Create a table
+    cursor.execute("CREATE TABLE trainer (id serial PRIMARY KEY, zeitraum varchar(100), name varchar(50));")
+    print("Finished creating table")
+
+    #  i = 0 
+    #  #Insert Dataframe into SQL Server:
+    #  for i, e in chunks:
+    #       cursor.execute("INSERT INTO trainer (zeitraum, name) values (%s, %s);", (i, e))
+          
+    #       i = i + 1
+    #       print(i)
+
+    cursor.executemany("INSERT INTO trainer (zeitraum, name) values (%s, %s);", chunks)
+    print("Finished inserting into table")
+
+trainer()
+
+# Clean up
+conn.commit()
+cursor.close()
+conn.close()
